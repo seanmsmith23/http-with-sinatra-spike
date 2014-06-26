7ef8d3d48c2375f00ad745b38ba608b03b6571b1
Sean Smith <seansmith1020@gmail.com>
2014-06-26 16:25:01 -0600

    End of first day

--- app.rb
+++ app.rb
@@ -1,17 +1,43 @@
 require "sinatra/base"
-require "sinatra/reloader"
+require_relative "ruby_template"
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
 
   def initialize
     super
-    @items = []
+    @items = [
+      "Hamburger",
+      "French Fries",
+      "Blocks of Cheese",
+    ]
   end
 
   get "/" do
-    "items length: #{@items.length}"
+    erb :root, :locals => { :number_items => item_length}
   end
 
+  get "/items" do
+    erb :items
+  end
+
+  get "/items/new" do
+    erb :new_item
+  end
+
+  post "/items" do
+    new_item = params[:eaten]
+    @items << new_item.to_s
+    erb :items
+  end
+
+  get "/items/:item_number" do
+     identifier = params[:item_number].to_i
+    "You ate: #{@items[identifier]}"
+  end
+
+
+
   run! if app_file == $0
+
 end
+
---
+++ ruby_template.rb
@@ -0,0 +1,4 @@
+def item_length
+  "Number of things you've eaten: #{@items.length}"
+end
+
